e75bbdbe68f1ebb3f87e9c472ada27b729701c11
xueqi <xueqi@cloudcc.com>
2020-07-15 18:03:46 +0800

    js底层机制

---
+++ js底层运行机制之堆(Heap)栈(Stack)内存.js
@@ -0,0 +1,19 @@
+// let a = 12;
+// let b = 13;
+
+// function func(){
+//   let a = 1;
+//   let b = 2;
+// }
+
+// func();
+
+// console.log(a,b);
+
+// var a={}, b='0',c=0;
+// a[b]='珠峰';  // a['0] = '珠峰'
+// a[c]='培训';  // a[0] = '培训'
+// console.log(a[b]);  // => "培训"
+
+let c={d:12}
+console.log(c['0'])
